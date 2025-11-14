9b441a1b17c19523b82e1c55cad84a63141c955d
svetoslavseo <svetoslavppc@gmail.com>
2025-11-14 21:17:13 +0000

    Update robots.txt to disallow URLs with ?v= query parameters and /_next/static/css/app/ paths

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -6,7 +6,15 @@ export default function robots(): MetadataRoute.Robots {
     rules: {
       userAgent: '*',
       allow: '/',
-      disallow: ['/api/', '/admin/'],
+      disallow: [
+        '/api/',
+        '/admin/',
+        // Disallow Next.js internal CSS module paths that may have version query parameters
+        // These paths like /_next/static/css/app/*.css?v=* return 404s and shouldn't be crawled
+        '/_next/static/css/app/',
+        // Disallow any URLs with ?v= query parameter (cache-busting parameters)
+        '/*?v=',
+      ],
     },
     sitemap: `${SITE_CONFIG.url}/sitemap.xml`,
   };
